3287df80c5d17a4e185a7bea6a441decaaca1fac
bgilman <bgilman@Ben-Gilman-Mac.local>
2015-08-24 22:03:35 -0500

    john-papa-rize directive template

--- templates/javascript/directive.js
+++ templates/javascript/directive.js
@@ -6,13 +6,23 @@
  * @description
  * # <%= cameledName %>
  */
-angular.module('<%= scriptAppName %>')
-  .directive('<%= cameledName %>', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the <%= cameledName %> directive');
-      }
-    };
-  });
+angular
+  .module('<%= scriptAppName %>')
+  .directive('<%= cameledName %>', <%= cameledName %>);
+
+function <%= cameledName %>() {
+  var directive = {
+    restrict: 'EA',
+    template: '<div></div>',
+    scope: true,
+    controller: <%= classedName %>Controller,
+    controllerAs: 'vm',
+    bindToController: true
+  };
+
+  return directive;
+}
+
+function <%= classedName %>Controller() {
+  var vm = this;
+}
